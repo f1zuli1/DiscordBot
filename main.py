c70cc93277a87f95a676a52f671e6e32e73b2adc
Fuzuli <fuzuliisr06@gmail.com>
2025-08-01 12:51:33 +0400

    main.py

--- main.py
+++ main.py
@@ -2,21 +2,63 @@ import discord
 from discord.ext import commands
 from config import token
 from logic import Pokemon
-import random
 from logic import Wizard,Fighter
+from logic import quiz_questions
+from collections import defaultdict
+from config import token
+import random
 # Bot için yetkileri/intents ayarlama
 intents = discord.Intents.default()  # Varsayılan ayarların alınması
 intents.messages = True              # Botun mesajları işlemesine izin verme
 intents.message_content = True       # Botun mesaj içeriğini okumasına izin verme
 intents.guilds = True                # Botun sunucularla çalışmasına izin verme
 
+intents = discord.Intents.default()
+intents.message_content = True
 # Tanımlanmış bir komut önekine ve etkinleştirilmiş amaçlara sahip bir bot oluşturma
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+user_responses = {}
+points = defaultdict(int)
 
 # Bot çalışmaya hazır olduğunda tetiklenen bir olay
 @bot.event
 async def on_ready():
     print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
+
+async def send_question(ctx_or_interaction, user_id):
+    question = quiz_questions[user_responses[user_id]]
+    buttons = question.gen_buttons()
+    view = discord.ui.View()
+    for button in buttons:
+        view.add_item(button)
+
+    if isinstance(ctx_or_interaction, commands.Context):
+        await ctx_or_interaction.send(question.text, view=view)
+    else:
+        await ctx_or_interaction.followup.send(question.text, view=view)
+#question
+@bot.event
+async def on_interaction(interaction):
+    user_id = interaction.user.id
+    if user_id not in user_responses:
+        await interaction.response.send_message("Lütfen !start komutunu yazarak testi başlatın")
+        return
+
+    custom_id = interaction.data["custom_id"]
+    if custom_id.startswith("correct"):
+        await interaction.response.send_message("Doğru cevap!")
+        points[user_id] +=1
+        # Görev 9 - Doğru cevap için kullanıcıya puan ekleyin
+    elif custom_id.startswith("wrong"):
+        await interaction.response.send_message("Yanlış cevap!")
+
+    user_responses[user_id]+=1
+    if user_responses[user_id] > len(quiz_questions) -1:
+        await interaction.followup.send(f"Tebrikler sinav bitti! Toplam Puaniniz {points}[user_id]")
+
+    else:
+        await send_question(interaction,user_id)
 
 # '!go' komutu
 @bot.command()
@@ -91,6 +133,11 @@ async def feed(ctx):
     else:
         await ctx.send("Pokémon'un yok!")
 
-
+@bot.command()
+async def start(ctx):
+    user_id = ctx.author.id
+    if user_id not in user_responses:
+        user_responses[user_id] = 0
+        await send_question(ctx, user_id)
 
 bot.run(token)
