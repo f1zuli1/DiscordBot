d7339530a819329bf722b779eeb7a31e6d03b215
Fuzuli <fuzuliisr06@gmail.com>
2025-08-01 12:50:58 +0400

    logic.py

--- logic.py
+++ logic.py
@@ -1,7 +1,32 @@
-import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
+import aiohttp
 import random
 import asyncio
 from datetime import datetime,timedelta
+from discord import ui,ButtonStyle
+class Question:
+    def __init__(self, text, answer_id, *options):
+        self.__text = text
+        self.__answer_id = answer_id
+        self.options = options
+
+    @property
+    def text(self):
+        return self.__text 
+    def gen_buttons(self):
+        buttons=[]
+        for i,option in enumerate(self.options):
+            if i==self.__answer_id:
+                buttons.append(ui.Button(label=option,style=ButtonStyle.primary,custom_id=f"correct_{i}"))
+            else:
+                buttons.append(ui.Button(label=option,style=ButtonStyle.primary,custom_id=f"wrong_{i}"))
+        return buttons
+quiz_questions = [
+   Question("Azerbaycan'ın başkenti neresidir?", 1, "Küba", "Bakü"),
+   Question("Hangisi Asya'da bulunur?", 0, "Azerbaycan", "ABD", "Almanya"),
+   Question("Galatasaray hangi yıl kuruldu ?", 3, "1907", "1903","1904", "1905"),
+   Question("Galatasaray takiminin renkleri", 1 , "sari, lacivert", "sari, kirmizi")
+]
+
 class Pokemon:
     pokemons = {}
     # Nesne başlatma (kurucu)
